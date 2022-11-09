8809017a3c1e8ed7e0553d5d78b3d4c2aa797602
yasmine-mmz <100961339+yasmine-mmz@users.noreply.github.com>
2022-11-09 21:09:00 +0100

    change bd name to espreat

--- ESPREAT/config.php
+++ ESPREAT/config.php
@@ -9,7 +9,7 @@ class config
         if (!isset(self::$pdo)) {
             try {
                 self::$pdo = new PDO(
-                    'mysql:host=localhost;dbname=ESPREAT',
+                    'mysql:host=localhost;dbname=espreat',
                     'root',
                     '',
                     [
