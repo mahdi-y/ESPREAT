fdc76892306acb846e5135f2fb05c99db425983c
mahdi-y <100856863+mahdi-y@users.noreply.github.com>
2022-11-11 23:34:47 +0100

    semehni ames

--- ESPREAT/View/Back/navbar.php
+++ ESPREAT/View/Back/navbar.php
@@ -33,7 +33,7 @@
                   
                     <a href="table.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
                     <a href="chart.php" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
-                    
+
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Products</a>
                         <div class="dropdown-menu bg-transparent border-0">
@@ -61,7 +61,6 @@
             </nav>
         </div>
         <!-- Sidebar End -->
-
 
         <!-- Content Start -->
         <div class="content">
