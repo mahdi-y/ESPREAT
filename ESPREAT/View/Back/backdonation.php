93aaca934e2fb790c35f452130ce04a55388eb04
iheb09 <101050710+iheb09@users.noreply.github.com>
2022-11-13 16:07:29 +0100

    update

--- ESPREAT/View/Back/backdonation.php
+++ ESPREAT/View/Back/backdonation.php
@@ -5,28 +5,32 @@
 <div class="container-fluid pt-4 px-4">
                 <div class="bg-secondary text-center rounded p-4">
                     <div class="d-flex align-items-center justify-content-between mb-4">
-                        <h6 class="mb-0">Association</h6>
-                        <a href="addproduct.php">Add association</a>
+                        <h6 class="mb-0">Donations</h6>
+                        <a href="addproduct.php">Add donation</a>
                     </div>
                     <div class="table-responsive">
                         <table class="table text-start align-middle table-bordered table-hover mb-0">
                             <thead>
                                 <tr class="text-white">
                                     <th scope="col"><input class="form-check-input" type="checkbox"></th>
-                                    <th scope="col">Nom</th>
-                                    <th scope="col">Numero</th>
-                                    <th scope="col">Adresse</th>
-                                    <th scope="col">President</th>
+                                    <th scope="col">idClient</th>
+                                    <th scope="col">classe</th>
+                                    <th scope="col">gender</th>
+                                    <th scope="col">montant TND</th>
+                                    <th scope="col">idDon</th>
+                                    <th scope="col">anonymat</th>
                                     
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>Croissant rouge</td>
-                                    <td>29 203 573</td>
-                                    <td>20 , rue d'afrique , charguia 5</td>
-                                    <td>Iheb Souissi</td>
+                                    <td>1454785</td>
+                                    <td>2A2</td>
+                                    <td>0</td>
+                                    <td>55</td>
+                                    <td>552457</td>
+                                    <td>0</td>
                             
                                     <td><a class="btn btn-sm btn-primary" href="productupdate.php?id=<?=$c ['idP'];?>">Update</a></td>
                                     <td><a class="btn btn-sm btn-primary" href="">Delete</a></td>
